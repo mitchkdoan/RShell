98c2a184c61f321f5b960cd37c5ee04ea39c1553
Mitchell Doan <32938116+mitchkdoan@users.noreply.github.com>
2020-03-05 21:39:35 -0800

    Add necessary files for CMake

---
+++ CMakeFiles/rshell.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ubuntu/workspace/CS100/assignment-1-bakugo-is-misunderstood/src/And.cpp" "/home/ubuntu/workspace/CS100/assignment-1-bakugo-is-misunderstood/CMakeFiles/rshell.dir/src/And.cpp.o"
+  "/home/ubuntu/workspace/CS100/assignment-1-bakugo-is-misunderstood/src/Command.cpp" "/home/ubuntu/workspace/CS100/assignment-1-bakugo-is-misunderstood/CMakeFiles/rshell.dir/src/Command.cpp.o"
+  "/home/ubuntu/workspace/CS100/assignment-1-bakugo-is-misunderstood/src/DOutRedirect.cpp" "/home/ubuntu/workspace/CS100/assignment-1-bakugo-is-misunderstood/CMakeFiles/rshell.dir/src/DOutRedirect.cpp.o"
+  "/home/ubuntu/workspace/CS100/assignment-1-bakugo-is-misunderstood/src/InRedirect.cpp" "/home/ubuntu/workspace/CS100/assignment-1-bakugo-is-misunderstood/CMakeFiles/rshell.dir/src/InRedirect.cpp.o"
+  "/home/ubuntu/workspace/CS100/assignment-1-bakugo-is-misunderstood/src/Or.cpp" "/home/ubuntu/workspace/CS100/assignment-1-bakugo-is-misunderstood/CMakeFiles/rshell.dir/src/Or.cpp.o"
+  "/home/ubuntu/workspace/CS100/assignment-1-bakugo-is-misunderstood/src/OutRedirect.cpp" "/home/ubuntu/workspace/CS100/assignment-1-bakugo-is-misunderstood/CMakeFiles/rshell.dir/src/OutRedirect.cpp.o"
+  "/home/ubuntu/workspace/CS100/assignment-1-bakugo-is-misunderstood/src/Pipe.cpp" "/home/ubuntu/workspace/CS100/assignment-1-bakugo-is-misunderstood/CMakeFiles/rshell.dir/src/Pipe.cpp.o"
+  "/home/ubuntu/workspace/CS100/assignment-1-bakugo-is-misunderstood/src/Semi.cpp" "/home/ubuntu/workspace/CS100/assignment-1-bakugo-is-misunderstood/CMakeFiles/rshell.dir/src/Semi.cpp.o"
+  "/home/ubuntu/workspace/CS100/assignment-1-bakugo-is-misunderstood/src/TestCmd.cpp" "/home/ubuntu/workspace/CS100/assignment-1-bakugo-is-misunderstood/CMakeFiles/rshell.dir/src/TestCmd.cpp.o"
+  "/home/ubuntu/workspace/CS100/assignment-1-bakugo-is-misunderstood/src/main.cpp" "/home/ubuntu/workspace/CS100/assignment-1-bakugo-is-misunderstood/CMakeFiles/rshell.dir/src/main.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ CMakeFiles/rshell.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/rshell.dir/src/main.cpp.o"
+  "CMakeFiles/rshell.dir/src/Command.cpp.o"
+  "CMakeFiles/rshell.dir/src/And.cpp.o"
+  "CMakeFiles/rshell.dir/src/Or.cpp.o"
+  "CMakeFiles/rshell.dir/src/TestCmd.cpp.o"
+  "CMakeFiles/rshell.dir/src/Semi.cpp.o"
+  "CMakeFiles/rshell.dir/src/InRedirect.cpp.o"
+  "CMakeFiles/rshell.dir/src/OutRedirect.cpp.o"
+  "CMakeFiles/rshell.dir/src/DOutRedirect.cpp.o"
+  "CMakeFiles/rshell.dir/src/Pipe.cpp.o"
+  "rshell.pdb"
+  "rshell"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/rshell.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
